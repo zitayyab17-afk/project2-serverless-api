a4284f1ce9ba8d111379aecbfcfa198d011aad9e
Muhammad Zeeshan Tayyab <zitayyab17@gmail.com>
2026-02-23 13:22:18 +0000

    Fix line endings for Terraform CI/CD

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket         = "project2-terraform-state-file"             # your S3 bucket name
-    key            = "project2-serverless-api/terraform.tfstate" # path inside bucket
-    region         = "eu-west-2"
-    dynamodb_table = "project2-terraform-lock"
-    encrypt        = true
-  }
-}
+terraform {  backend "s3" {    bucket         = "project2-terraform-state-file"             # your S3 bucket name    key            = "project2-serverless-api/terraform.tfstate" # path inside bucket    region         = "eu-west-2"    dynamodb_table = "project2-terraform-lock"    encrypt        = true  }}
--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,14 +1,1 @@
-resource "aws_dynamodb_table" "products" {
-  name         = "ProductTable"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "productId"
-
-  attribute {
-    name = "productId"
-    type = "S"
-  }
-
-  tags = {
-    Name = "ProductTable-Terraform"
-  }
-}
+resource "aws_dynamodb_table" "products" {  name         = "ProductTable"  billing_mode = "PAY_PER_REQUEST"  hash_key     = "productId"  attribute {    name = "productId"    type = "S"  }  tags = {    Name = "ProductTable-Terraform"  }}
